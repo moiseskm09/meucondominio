3e4accb79a449964111f3badbeb7d05b745b5853
moises <moises@emk>
2021-04-16 00:01:27 -0300

    checklist

---
+++ sistema/checklist.php
@@ -0,0 +1,39 @@
+<?php
+require_once '../config/sessao.php';
+require_once '../config/conexao.php';
+require_once '../config/config_geral.php';
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <link href="../css/menu.css" rel="stylesheet" />
+        <link href="../css/geral.css" rel="stylesheet" />
+        <link rel="stylesheet" href="../css/bootstrap.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
+        <script src="../js/jquery-3.1.1.js"></script>
+        <script src="../js/mask.js"></script>
+        <script src="../js/loading.js"></script>
+    </head>
+    <body class="sb-nav-fixed">
+        <?php include_once "../ferramentas/navbar.php"; ?>
+        <div id="layoutSidenav">
+            <?php include_once "../ferramentas/menu.php"; ?>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid">
+                        <!--conteudo da tela aqui!-->
+ 
+                        <!--fim conteudo da tela aqui!-->
+                </main>
+                <?php include_once "../ferramentas/rodape.php"; ?>
+            </div>
+        </div>
+        <script src="../js/cp_mascaras.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="../js/scripts.js"></script>
+    </body>
+</html>
